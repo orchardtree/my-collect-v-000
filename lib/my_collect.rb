0571eca5a909f4bcabbba0799b14af718828d5eb
Jesse Wenger <wengerj@gmail.com>
2021-03-12 17:13:21 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
+def my_collect(students)
+  i = 0
+  while i < array.length
+    yield(array[i])
+    i += 1
+  end
+  array
+end
 
+my_collect(["Tim Jones", "Tom Smith", "Jim Campagno"]) do |name|
 
